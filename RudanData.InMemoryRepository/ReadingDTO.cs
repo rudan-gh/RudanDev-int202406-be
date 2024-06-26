902c027974440ad04d2eadf524db2e72ddf512a2
Matea <mateavuceticdrndic@gmail.com>
2024-06-26 20:45:30 +0200

    Testni zadatak Matea Vučetić Drndić

--- RudanData.InMemoryRepository/ReadingDTO.cs
+++ RudanData.InMemoryRepository/ReadingDTO.cs
@@ -3,7 +3,12 @@ namespace RudanData.Entities
 {
     public class ReadingDTO(ReadingModel model)
     {
-        public long Timestamp { get; set; } = ((DateTimeOffset)model.ReadingDate).ToUnixTimeSeconds();
+        // Ispravak pretvorbe iz sekunde u milisekunde
+        public long Timestamp { get; set; } = ((DateTimeOffset)model.ReadingDate).ToUnixTimeMilliseconds();
+
         public double? Value { get; set; } = model.ReadingValue?.Value;
+
+        //Dodano svojstvo IsExtrapolated
+        public bool? IsExtrapolated { get; set; } = model.ReadingValue?.IsExtrapolated;
     }
 }
